ef78253ab63183c4d6c9142066a96db308a286a6
EC2 Default User <ec2-user@ip-172-31-44-235.ap-northeast-1.compute.internal>
2019-06-06 08:32:35 +0000

    add main

---
+++ app/controllers/ctrlcv_controller.rb
@@ -0,0 +1,7 @@
+class CtrlcvController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    
+    def main
+        
+    end
+end
---
+++ app/helpers/ctrlcv_helper.rb
@@ -0,0 +1,2 @@
+module CtrlcvHelper
+end
